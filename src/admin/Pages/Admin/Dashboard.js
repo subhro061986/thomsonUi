9bd88fa4b2bf5131569e192dbecf3a920c5bd8c9
Jafrin parvin <jafrinparvin2002@gmail.com>
2024-08-01 16:11:03 +0530

    dashboard design started

--- src/admin/Pages/Admin/Dashboard.js
+++ src/admin/Pages/Admin/Dashboard.js
@@ -26,113 +26,85 @@ const Dashboard = () => {
             <SideMenu />
             <div className="wrapper d-flex flex-column min-vh-100 bg-light">
                 <Header title="Dashboard" />
-                <div className="">
-                    <div class="container overflow-hidden">
-                        <div class="row gx-3">
-                            <div class="col-4">
-                                {/* <div class="p-3 border bg-light">Custom column padding</div> */}
-                                <div class="card-body p-3 border bg-white">
-                                    <h5 class="card-title">Customer Sales</h5>
-                                    {/* <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6> */}
-                                    <table class="table">
-                                        <thead>
-                                            <tr>
-                                                <th scope="col"></th>
-                                                <th scope="col">Customer id</th>
-                                                <th scope="col">Last Month</th>
-                                                <th scope="col">Current Month</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                                <th scope="row">1</th>
-                                                <td>Mark</td>
-                                                <td>Otto</td>
-                                                <td>@mdo</td>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row">2</th>
-                                                <td>Jacob</td>
-                                                <td>Thornton</td>
-                                                <td>@fat</td>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row">3</th>
-                                                <td colspan="2">Larry the Bird</td>
-                                                <td>@twitter</td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
+                {/* <div className="row m-3 bg-white p-2">
+
+                    <div className="col-md-8 book-details p-3" >
+                        abcd
+                    </div>
+
+                    <div className="col-md-4">
+                        abcd
+                    </div>
+                </div> */}
+                <div className="row d-flex justify-content-between m-3  p-2">
+                <div className="col-md-4">
+                        <div className="card-body p-3 border bg-white rounded">
+                            <h5 className="card-title">
+                                <select classNameName="form-select mb-3"
+                                    style={{ width: "100%" }}
+                                >
+                                    <option disabled value={0}>Please select</option>
+                                    <option
+                                    >cart title</option>
+
+                                </select>
+                            </h5>
+                            <div className="row mt-2  " style={{ marginLeft: "1px", marginRight: "1px" }} >
+
+                                <div className="col-md-6 border-end border-secondary h100 "  >
+
+                                    <h5>Title</h5>
+                                </div>
+                                <div className="col-md-6  h100" >
+                                    <h5>Title</h5>
                                 </div>
                             </div>
-                            <div class="col-4">
-                                <div class="card-body p-3 border bg-white">
-                                    <h5 class="card-title">Distributor Sales</h5>
-                                    <h6 class="card-subtitle mb-2 text-muted">Yearly Sales</h6>
-                                    <table class="table">
-                                        <thead>
-                                            <tr>
-                                                <th scope="col"></th>
-                                                <th scope="col">Distributor id</th>
-                                                <th scope="col">Last Month</th>
-                                                <th scope="col">Current Month</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                                <th scope="row">1</th>
-                                                <td>Mark</td>
-                                                <td>Otto</td>
-                                                <td>@mdo</td>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row">2</th>
-                                                <td>Jacob</td>
-                                                <td>Thornton</td>
-                                                <td>@fat</td>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row">3</th>
-                                                <td colspan="2">Larry the Bird</td>
-                                                <td>@twitter</td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="card-body p-3 border bg-white rounded">
+                            <h5 className="card-title">
+                                <select classNameName="form-select mb-3"
+                                    style={{ width: "100%" }}
+                                >
+                                    <option disabled value={0}>Please select</option>
+                                    <option
+                                    >cart title</option>
+
+                                </select>
+                            </h5>
+                            <div className="row mt-2  " style={{ marginLeft: "1px", marginRight: "1px" }} >
+
+                                <div className="col-md-6 border-end border-secondary h100 "  >
+
+                                    <h5>Title</h5>
+                                </div>
+                                <div className="col-md-6  h100" >
+                                    <h5>Title</h5>
                                 </div>
                             </div>
-                            <div class="col-4">
-                                <div class="card-body p-3 border bg-white">
-                                    <h5 class="card-title">Publisher Sales</h5>
-                                    {/* <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6> */}
-                                    <table class="table">
-                                        <thead>
-                                            <tr>
-                                                <th scope="col"></th>
-                                                <th scope="col">Publisher id</th>
-                                                <th scope="col">Last Month</th>
-                                                <th scope="col">Current Month</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                                <th scope="row">1</th>
-                                                <td>Mark</td>
-                                                <td>Otto</td>
-                                                <td>@mdo</td>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row">2</th>
-                                                <td>Jacob</td>
-                                                <td>Thornton</td>
-                                                <td>@fat</td>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row">3</th>
-                                                <td colspan="2">Larry the Bird</td>
-                                                <td>@twitter</td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="card-body p-3 border bg-white rounded">
+                            <h5 className="card-title">
+                                <select classNameName="form-select mb-3"
+                                    style={{ width: "100%" }}
+                                >
+                                    <option disabled value={0}>Please select</option>
+                                    <option
+                                    >cart title</option>
+
+                                </select>
+                            </h5>
+                            <div className="row mt-2  " style={{ marginLeft: "1px", marginRight: "1px" }} >
+
+                                <div className="col-md-6 border-end border-secondary h100 "  >
+
+                                    <h5>Title</h5>
+                                </div>
+                                <div className="col-md-6  h100" >
+                                    <h5>Title</h5>
                                 </div>
                             </div>
                         </div>
