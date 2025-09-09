c5577a21a50e3c514832b2209e3a3d7c5d98ee90
puja1319 <pujadey1319@gmail.com>
2025-09-09 12:59:53 +0530

    title added to title and author

--- src/Layout/PublishersBooks.js
+++ src/Layout/PublishersBooks.js
@@ -135,12 +135,12 @@ const PublishersBooks = () => {
                                                     loading="lazy"
                                                 />
                                             </div>
-                                            <div className="d-flex justify-content-center book_name mx-2 mt-2">
+                                            <div className="d-flex justify-content-center book_name mx-2 mt-2" title={book.title}>
                                                 {book.title.length > 10 ? book.title.substring(0, 10) + ".." : book.title}
                                                 {/* Insurance Coverage of Construction Disputes */}
                                             </div>
                                             {/* <div className="d-flex justify-content-center pub_name mt-2">Publisher: <span className="pub_span">{data.publisher.length > 20 ? data.publisher.substring(0, 20) + "..." : data.publisher}</span></div> */}
-                                            <div className="d-flex justify-content-center author_name">
+                                            <div className="d-flex justify-content-center author_name" title={book.authors}>
                                                 Author:
                                                 {/* Name */}
                                                 {book.authors.length > 15 ? book.authors.substring(0, 15) + "..." : book.authors}
